b96a6429058949286afa416a9c06a8850755859a
abishekrkashyap <abishekraghvan03@gmail.com>
2020-06-20 01:13:02 +0530

    Updates

---
+++ precache-manifest.40407528c49dc00119710750e99b277a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640e5b3d27e44e56358d474ce431aa63",
+    "url": "/covid19-Tracker.git/index.html"
+  },
+  {
+    "revision": "0f5be456a39edd9bd46b",
+    "url": "/covid19-Tracker.git/static/css/main.6480fb5a.chunk.css"
+  },
+  {
+    "revision": "03c9e65195b189324193",
+    "url": "/covid19-Tracker.git/static/js/2.835257d1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-Tracker.git/static/js/2.835257d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5be456a39edd9bd46b",
+    "url": "/covid19-Tracker.git/static/js/main.b480c37d.chunk.js"
+  },
+  {
+    "revision": "258c9ac1c2b26d8cff7f",
+    "url": "/covid19-Tracker.git/static/js/runtime-main.816f6855.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-Tracker.git/static/media/image.d7265326.png"
+  }
+]);
